a3e9d6448b05a087fb79e3d7f3e33403a501b7fa
Ahmed <Ahmed@DESKTOP-N2130HK>
2023-08-29 19:09:59 +0500

    Order Management

---
+++ Migrations/20230825171351_FKerrorinOrderHeaderModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    public partial class FKerrorinOrderHeaderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_UserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicaitonUserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "OrderHeaders",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderHeaders_UserId",
+                table: "OrderHeaders",
+                newName: "IX_OrderHeaders_ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "OrderHeaders",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                newName: "IX_OrderHeaders_UserId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicaitonUserId",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_UserId",
+                table: "OrderHeaders",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
